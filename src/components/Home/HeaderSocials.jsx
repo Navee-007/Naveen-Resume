7d86e9a384317f0bc1414743cc567cff248423e6
dina <naveenkumar.coder@gmail.com>
2023-03-30 15:41:00 +0530

    icon delete

--- src/components/Home/HeaderSocials.jsx
+++ src/components/Home/HeaderSocials.jsx
@@ -4,14 +4,12 @@ const HeaderSocials = () => {
   return (
     <div className='home-socials'>
       <a href="https://github.com/Navee-007" className='home-social-link' target="_blank">
-      <i className="fa-brands fa-github" />
+        <i className="fa-brands fa-github" />
       </a>
       <a href="https://www.linkedin.com/in/naveen-kumar-a76729263/" className='home-social-link' target="_blank">
-      <i className="fa-brands fa-linkedin" />
+        <i className="fa-brands fa-linkedin" />
       </a>
-      <a href="https://app.netlify.com/teams/navee-007/overview" className='home-social-link' target="_blank">
-      <i className="fa-solid fa-link" />
-      </a>
+
     </div>
   )
 }
